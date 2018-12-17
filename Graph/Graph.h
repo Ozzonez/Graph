f0cc389b964252934e70cdfa15b93b0fdde8f3d8
Daniel <recondrone7@gmail.com>
2018-12-17 22:50:51 +0100

    beginning

---
+++ Graph/Graph.h
@@ -0,0 +1,17 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+#include <iostream>
+
+class Gaph
+{
+    struct vertex
+    {
+    vertex * next;
+    int data;
+    };
+    public:
+
+
+};
+
+#endif // GRAPH_H
